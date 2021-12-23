9c86eff1c65f9b64af29dca5c74d68296a1cc2d8
Nur Kholiq Ansori <74647127+nurkholiqansori@users.noreply.github.com>
2021-12-23 19:15:55 +0700

    Add files via upload

---
+++ _next/static/0vvmfgblBdKKhUQpCWiAw/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fgithub","\u002Fabout","\u002Fwp-collection","\u002Fportofolio"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
---
+++ myAuthor.js
@@ -0,0 +1,129 @@
+export const contentData = {
+  id: 1,
+  myName: 'Nur Kholiq Ansori',
+  birthPlace: 'Bekasi, 25 April 2001',
+  address: '',
+  num: '+6285156421261',
+  mail: 'nurkholiqansori8@gmail.com',
+  socMed1: [
+    {
+      id: 1,
+      name: 'LinkedIn',
+      link: 'http://www.linkedin.com/in/nurkholiqansori',
+    },
+    {
+      id: 2,
+      name: 'Github',
+      link: 'https://github.com/nurkholiqansori',
+    },
+    {
+      id: 3,
+      name: 'SoloLearn',
+      link: 'https://www.sololearn.com/profile/3200321',
+    },
+  ],
+  socMed2: [
+    {
+      id: 1,
+      name: 'Facebook',
+      link: 'Https://fb.com/nurkholiq.ansori.1',
+    },
+    {
+      id: 2,
+      name: 'Twitter',
+      link: 'https://twitter.com/Enjoykudasai',
+    },
+    {
+      id: 3,
+      name: 'Instagram',
+      link: 'https://www.instagram.com/nurkholiqansori',
+    },
+  ],
+
+  desc: 'Seseorang yang menyukai web sejak Sekolah Menengah Pertama dan suka mengedit CSS, HTML sendiri. Biasa memakai ReactJS dan NextJS dan juga mencoba React Native. Saya biasa membuat web profil perusahaan.',
+
+  experience: [
+    {
+      id: 6,
+      title: 'Web Developer',
+      com: 'Karunia Kitchen Set',
+      date: 'Des 2021',
+      status: '',
+      address: 'https://www.karuniakitchensetbekasi.com/',
+      logo: '/doc/Logo/karuniakitchenset.png',
+      img: '',
+    },
+    {
+      id: 5,
+      title: 'Web Developer',
+      com: 'Mitra Java Sinergi',
+      date: 'Des 2021',
+      status: '',
+      address: 'https://www.mitraboltingrental.com/',
+      logo: '/doc/Logo/mitrajavasinergi.png',
+      img: '/doc/mitraboltingrental.png',
+    },
+    {
+      id: 4,
+      title: 'Web Developer',
+      com: 'Jasa Sedot WC Medan',
+      date: 'Okt 2021',
+      status: false,
+      address: 'https://www.jasasedotwc-medan.com',
+      logo: '/doc/Logo/sedotwc.png',
+      img: '/doc/jasasedotwc.png',
+    },
+    {
+      id: 3,
+      title: 'Web Developer',
+      com: 'YASSIN',
+      date: 'Okt 2021',
+      status: false,
+      address: 'https://www.yassin.or.id',
+      logo: '/doc/Logo/yassin.png',
+      img: '/doc/yassin.png',
+    },
+    {
+      id: 2,
+      title: 'Web Developer',
+      com: 'SMA FG Putri FDS',
+      date: 'Agu 2021',
+      status: true,
+      address: 'https://smafgputrifds.sch.id',
+      logo: '/doc/Logo/smafgputri.png',
+      img: '/doc/smafgputrifds.png',
+    },
+    {
+      id: 1,
+      title: 'Web Developer',
+      com: 'Ta’limy Ali bin Abi Thalib',
+      date: 'Sep 2020 hingga Mar 2021',
+      status: true,
+      address: 'https://talimyabat.id',
+      logo: '/doc/Logo/At-Talimy.png',
+      img: '/doc/talimyabat.png',
+    },
+  ],
+  study: [
+    {
+      id: 1,
+      title: 'S1 Teknik Informatika',
+      sch: 'Universitas Mitra Karya Bekasi',
+      date: 'Agu 2019 > Saat Ini',
+    },
+    {
+      id: 2,
+      title: 'IPS',
+      sch: 'SMAN 8 Bekasi',
+      date: '2016 > 2019',
+    },
+  ],
+  skill: [
+    {
+      id: 1,
+      title:
+        'HTML Fundamentals, PHP Fundamentals, CSS Fundamentals dan JavaScript',
+      desc: 'Belajar dari aplikasi android Sololearn sejak 2017. ',
+    },
+  ],
+}
